9a76c8a32d85fca8e0b9e227b4c5822c46d8586f
harshadsph <harshadsph@gmail.com>
2013-10-29 20:08:52 +0800

    commit

--- sites/all/themes/utsav/template.php
+++ sites/all/themes/utsav/template.php
@@ -14,7 +14,19 @@
  function utsav_preprocess_page(&$variables) {
   // Get the entire main menu tree
   $main_menu_tree = menu_tree_all_data('main-menu');
+  
+  //Get the Theme path 
+  $theme_path = drupal_get_path('theme', 'utsav');
 
   // Add the rendered output to the $main_menu_expanded variable
   $variables['main_menu_expanded'] = menu_tree_output($main_menu_tree);
+  
+  //If front page, then add the javascript for home page slider.  
+  if($variables['is_front']){
+		drupal_add_css("$theme_path/css/camera.css");
+		drupal_add_js("$theme_path/js/jquery.mobile.customized.min.js");
+		drupal_add_js("$theme_path/js/jquery.easing.1.3.js");
+		drupal_add_js("$theme_path/js/camera.min.js");
+  }
+  
 }
