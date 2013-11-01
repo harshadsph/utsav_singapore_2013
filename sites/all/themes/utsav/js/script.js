e60aba3ddf1f48f9d3fb37cc787a047ea71f98fd
Harshad <Harshadsph@gmail.com>
2013-11-01 18:49:04 +0800

    01 Nov - Commit

--- sites/all/themes/utsav/js/script.js
+++ sites/all/themes/utsav/js/script.js
@@ -29,6 +29,32 @@ Drupal.behaviors.homepage_image_video_slideshow = {
 			}
 		}
 	};
-
+	
+Drupal.behaviors.homepage_floating_footer = {
+		attach: function(context) {
+			floatingMenu.add('floatdiv',  
+			{  
+				// Represents distance from left or right browser window  
+				// border depending upon property used. Only one should be  
+				// specified.  
+				 targetLeft: 10,  
+				// targetRight: 10,  
+	  
+				// Represents distance from top or bottom browser window  
+				// border depending upon property used. Only one should be  
+				// specified.  
+				//targetTop: 10,  
+				targetBottom: 410,  
+	  
+				// Uncomment one of those if you need centering on  
+				// X- or Y- axis.  
+				 //centerX: true,  
+				 //centerY: true,  
+	  
+				// Remove this one if you don't want snap effect  
+				snap: true  
+			}); 
+		}
+};
 
 })(jQuery);
